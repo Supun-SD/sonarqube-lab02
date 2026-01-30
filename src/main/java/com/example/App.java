8c1bb8e3140ccbc4cfe2be54bf75a0c992c9ac49
Supun-SD <supundunusingha2@gmail.com>
2026-01-31 00:05:10 +0530

    Fix quality issues

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,9 +2,11 @@ package main.java.com.example;
 
 public class App {
 
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(App.class.getName());
+
     public static void main(String[] args) throws Exception {
         Calculator calc = new Calculator();
-        System.out.println(calc.calculate(10, 5, "add-again"));
+        logger.info(String.valueOf(calc.calculate(10, 5, "add-again")));
         UserService service = new UserService();
         service.findUser("admin");
         service.deleteUser("admin"); // NEW dangerous call
