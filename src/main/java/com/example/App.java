fbcf11683850a514260e13597ddc657137b21434
Supun-SD <supundunusingha2@gmail.com>
2026-01-31 00:09:04 +0530

    Fix quality issues

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,7 +6,9 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Calculator calc = new Calculator();
-        logger.info(String.valueOf(calc.calculate(10, 5, "add-again")));
+        if (logger.isLoggable(java.util.logging.Level.INFO)) {
+            logger.info(String.valueOf(calc.calculate(10, 5, "add-again")));
+        }
         UserService service = new UserService();
         service.findUser("admin");
         service.deleteUser("admin"); // NEW dangerous call
